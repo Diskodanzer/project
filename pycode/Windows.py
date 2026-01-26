6d9dcdc5e0c575168dc395de76e745064db15e75
StillerGH <StillerGames2007@gmail.com>
2026-01-26 17:59:22 +0300

    Update Windows.py

--- pycode/Windows.py
+++ pycode/Windows.py
@@ -2,30 +2,28 @@ import arcade
 from arcade.gui import UIManager, UIFlatButton, UITextureButton, UILabel, UIInputText, UITextArea, UISlider, UIDropdown, \
     UIMessageBox  # Это разные виджеты
 from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout  # А это менеджеры компоновки, как в pyQT
-from prototype import GridGame
+# from prototype import GridGame
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
 class MainWindow(arcade.Window):
-    def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Супер GUI Пример!")
+    def __init__(self, x=800, y=600):
+        self.x = x
+        self.y = y
+        super().__init__(self.x, self.y, "vymluva")
         arcade.set_background_color(arcade.color.BLACK)
 
-        # UIManager — сердце GUI
+
         self.manager = UIManager()
-        self.manager.enable()  # Включить, чтоб виджеты работали
+        self.manager.enable()
+        self.anchor_layout = UIAnchorLayout()
+        self.box_layout = UIBoxLayout(vertical=True, space_between=10)
+        self.setup_widgets()
 
-        # Layout для организации — как полки в шкафу
-        self.anchor_layout = UIAnchorLayout()  # Центрирует виджеты
-        self.box_layout = UIBoxLayout(vertical=True, space_between=10)  # Вертикальный стек
-
-        # Добавим все виджеты в box, потом box в anchor
-        self.setup_widgets()  # Функция ниже
-
-        self.anchor_layout.add(self.box_layout)  # Box в anchor
-        self.manager.add(self.anchor_layout)  # Всё в manager
+        self.anchor_layout.add(self.box_layout)
+        self.manager.add(self.anchor_layout)
 
 
 
@@ -38,50 +36,47 @@ class MainWindow(arcade.Window):
         self.box_layout.add(label)
 
         flat_button = UIFlatButton(text="Начать", width=200, height=50, color=arcade.color.RED)
-        flat_button.on_click = lambda event: self.open_game()  # Не только лямбду, конечно
+        flat_button.on_click = lambda event: self.open_game()
         self.box_layout.add(flat_button)
 
         flat_button1 = UIFlatButton(text="Настройки", width=200, height=50, color=(0, 0, 0))
-        flat_button1.on_click = lambda event: self.open_settings()  # Не только лямбду, конечно
+        flat_button1.on_click = lambda event: self.open_settings()
         self.box_layout.add(flat_button1)
-        # Здесь добавим ВСЕ виджеты — по порядку! # Заполним ниже
 
     def on_draw(self):
         self.clear()
-        self.manager.draw()  # Рисуй GUI поверх всего
+        self.manager.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        pass  # Для кликов, но manager сам обрабатывает
+        pass
 
     def on_button_clicked(self):
         pass
 
     def open_game(self):
-        game = GridGame()
+        game = GridGame(self.x, self.y)
         self.window.show_view(game)
 
     def open_settings(self):
-        win = SettingsWindow()
+        win = SettingsWindow(self.x, self.y)
         self.show_view(win)
 
 class SettingsWindow(arcade.Window):
-    def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Супер GUI Пример!")
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        super().__init__(self.x, self.y, "vymluva: настройки")
         arcade.set_background_color(arcade.color.BLACK)
 
-        # UIManager — сердце GUI
+
         self.manager = UIManager()
-        self.manager.enable()  # Включить, чтоб виджеты работали
+        self.manager.enable()
+        self.anchor_layout = UIAnchorLayout()
+        self.box_layout = UIBoxLayout(vertical=True, space_between=10)
+        self.setup_widgets()
 
-        # Layout для организации — как полки в шкафу
-        self.anchor_layout = UIAnchorLayout()  # Центрирует виджеты
-        self.box_layout = UIBoxLayout(vertical=True, space_between=10)  # Вертикальный стек
-
-        # Добавим все виджеты в box, потом box в anchor
-        self.setup_widgets()  # Функция ниже
-
-        self.anchor_layout.add(self.box_layout)  # Box в anchor
-        self.manager.add(self.anchor_layout)  # Всё в manager
+        self.anchor_layout.add(self.box_layout)
+        self.manager.add(self.anchor_layout)
 
 
 
@@ -101,41 +96,32 @@ class SettingsWindow(arcade.Window):
         self.box_layout.add(label)
 
         dropdown = UIDropdown(options=["1080 x 720", "800 x 600", "1440 x 1080"], width=200)
-        dropdown.on_change = lambda value: print(f"Выбрано: {str(value)}")
+        dropdown.on_change = lambda value: self.xy(str(value).split(' x '))
         self.box_layout.add(dropdown)
+
+        flat_button1 = UIFlatButton(text="Назад", width=200, height=50, color=(0, 0, 0))
+        flat_button1.on_click = lambda event: self.open_game()  # Не только лямбду, конечно
+        self.box_layout.add(flat_button1)
 
     def on_draw(self):
         self.clear()
-        self.manager.draw()  # Рисуй GUI поверх всего
+        self.manager.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        pass  # Для кликов, но manager сам обрабатывает
-
-    def on_button_clicked(self):
         pass
+    
+    def xy(self, mass):
+        self.x = mass[0]
+        self.y = mass[1]
 
     def open_game(self):
-        print('fg')
-
-    def open_settings(self):
-        print('settings')
+        game = MainWindow(self.x, self.y)
+        self.window.show_view(game)
 
 
 def main():
-    # Создаём экземпляр нашего окна (800×600 пикселей, заголовок «Arcade Первый Контакт»)
     window = MainWindow()
-    # Вызываем setup() для инициализации игровых объектов
     window.run()
-    # Запускаем игровой цикл! Окно будет работать, пока его не закроют
-    arcade.run()
-
-
-def main():
-    # Создаём экземпляр нашего окна (800×600 пикселей, заголовок «Arcade Первый Контакт»)
-    window = MainWindow()
-    # Вызываем setup() для инициализации игровых объектов
-    window.run()
-    # Запускаем игровой цикл! Окно будет работать, пока его не закроют
     arcade.run()
 
 
